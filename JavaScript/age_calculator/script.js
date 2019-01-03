487ec980d353c52cf033124c2c3ccc4b52bb1985
Shambre Sena-Wright <ssenawright@techtonicgroup.com>
2019-01-02 23:14:34 -0700

    Changes the calculator to reflect leap years in the calculation.

--- JavaScript/age_calculator/script.js
+++ JavaScript/age_calculator/script.js
@@ -1,4 +1,6 @@
 var ageInYears = prompt("How old are you?");
-var ageInDays = ageInYears * 365;
+var ageInDays = ageInYears * 365.25;
+
+// Adding the .25 factors in a leap year for every four years of our calculation.
 
 alert(ageInYears + " years is roughly " + ageInDays + " days.");
