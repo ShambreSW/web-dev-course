bfcdf117b40d9c116d3976a063ab0ca4a9b7f571
Shambre Sena-Wright <ssenawright@techtonicgroup.com>
2019-01-02 22:59:37 -0700

    Commits and pushes one change to make the code cleaner by combining name variables.

--- JavaScript/js_stalker_exercise/script.js
+++ JavaScript/js_stalker_exercise/script.js
@@ -4,6 +4,9 @@ var lastName = prompt("What is your last name?");
 
 var age = prompt("How old are you?");
 
-console.log("Your full name is " + firstName + " " + lastName + ".");
+// For cleaner code, combine the firstName and lastName variable to create a fullName:
+var fullName = firstName + " " + lastName;
+
+console.log("Your full name is " + fullName + ".");
 
 console.log("You are " + age + " years old.");
